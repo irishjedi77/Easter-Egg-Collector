d084beddbb5b85d8636d71ec07a6981c60f6c8a8
Erin Lyden <irishjedi77@mac.com>
2019-04-21 22:23:01 -0700

    updated

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    var randNum = Math.floor(Math.random() * 100) + 1;
+    $("#randomNumber").text(randNum);
+    console.log(randNum);
+
+
+
+    //let green = Math.floor(Math.random() * 25) + 1;
+    //let yellow = Math.floor(Math.random() * 25) + 1;
+    //let blue = Math.floor(Math.random() * 25) + 1;
+    //let purple = Math.floor(Math.random() * 25) + 1;
+
+
+
+});
